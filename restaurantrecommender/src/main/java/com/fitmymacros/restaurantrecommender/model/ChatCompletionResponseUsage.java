fb0ce00cb6d06de6b70e714dd7a1fbfea713a5f4
Pablo <pabloskixd23@gmail.com>
2024-07-17 00:15:33 +0200

    initial code

---
+++ restaurantrecommender/src/main/java/com/fitmymacros/restaurantrecommender/model/ChatCompletionResponseUsage.java
@@ -0,0 +1,7 @@
+package com.fitmymacros.restaurantrecommender.model;
+
+public class ChatCompletionResponseUsage {
+    private Integer promptTokens;
+    private Integer completionTokens;
+    private Integer totalTokens;
+}
